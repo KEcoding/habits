8113ca45ab848c422aa1b0beff31fe6433c65f03
Christopher Su <christophersu9@gmail.com>
2015-03-05 00:09:56 -0800

    api

--- server.py
+++ server.py
@@ -1,14 +1,44 @@
 from flask import Flask
-from flask.ext import restful
+from flask.ext.restful import Resource, Api, reqparse
+import dataset
+
+db = dataset.connect('sqlite:///habits.db')
+db['habits'].create_index(['slug'])
+db['entries'].create_index(['date'])
 
 app = Flask(__name__)
-api = restful.Api(app)
+api = Api(app)
 
-class HelloWorld(restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
+class Habit(Resource):
+    def get(self, slug):
+        return db['habits'].find_one(slug=slug)
 
-api.add_resource(HelloWorld, '/')
+    def post(self, slug):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        args = parser.parse_args()
+
+        habit_id = db['habits'].insert(dict(name=args['name'], slug=slug))
+        return db['habits'].find_one(id=habit_id)
+
+class Entry(Resource):
+    def get(self, date):
+        return list(db['entries'].find(date=date))
+
+    def post(self, date):
+        parser = reqparse.RequestParser()
+        parser.add_argument('habit', type=str, action='append', required=True)
+        args = parser.parse_args()
+
+        entry = dict()
+        entry['date'] = date
+        for habit in args['habit']:
+            entry[habit] = True
+        entry_id = db['entries'].insert(entry)
+        return db['entries'].find_one(id=entry_id)
+
+api.add_resource(Habit, '/<string:slug>')
+api.add_resource(Entry, '/entries/<string:date>')
 
 if __name__ == '__main__':
     app.run(debug=True)
