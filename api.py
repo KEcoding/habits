033cee8de4ec81a7636487bb0e5b6ee40b7505fb
Christopher Su <christophersu9@gmail.com>
2015-03-05 10:03:13 -0800

    api more or less done

---
+++ api.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, abort
+from flask.ext.restful import Resource, Api, reqparse
+import dataset
+
+db = dataset.connect('sqlite:///habits.db')
+db['habits'].create_index(['slug'])
+db['entries'].create_index(['date'])
+
+api_blueprint = Blueprint('api', __name__)
+api = Api(api_blueprint)
+
+def get_habits():
+    return [i['slug'] for i in list(db['habits'].all())]
+
+class Habit(Resource):
+    def get(self, slug):
+        return db['habits'].find_one(slug=slug)
+
+    def post(self, slug):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        args = parser.parse_args()
+
+        habit_id = db['habits'].insert(dict(name=args['name'], slug=slug))
+        return db['habits'].find_one(id=habit_id)
+
+class Entry(Resource):
+    def get(self, date):
+        return list(db['entries'].find(date=date))
+
+    def post(self, date):
+        parser = reqparse.RequestParser()
+        parser.add_argument('habit', type=str, action='append', required=True)
+        args = parser.parse_args()
+
+        entry = dict()
+        entry['date'] = date
+        habits = get_habits()
+        for habit in args['habit']:
+            if habit not in habits:
+                abort(400)
+        for habit in habits:
+            entry[habit] = (habit in args['habit'])
+
+        db['entries'].upsert(entry, ['date'])
+        return db['entries'].find_one(date=date)
+
+api.add_resource(Habit, '/<string:slug>')
+api.add_resource(Entry, '/entries/<string:date>')
--- server.py
+++ server.py
@@ -1,44 +1,12 @@
-from flask import Flask
-from flask.ext.restful import Resource, Api, reqparse
-import dataset
-
-db = dataset.connect('sqlite:///habits.db')
-db['habits'].create_index(['slug'])
-db['entries'].create_index(['date'])
+from flask import Flask, render_template
+from api import api_blueprint
 
 app = Flask(__name__)
-api = Api(app)
+app.register_blueprint(api_blueprint, url_prefix='/api')
 
-class Habit(Resource):
-    def get(self, slug):
-        return db['habits'].find_one(slug=slug)
-
-    def post(self, slug):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str, required=True)
-        args = parser.parse_args()
-
-        habit_id = db['habits'].insert(dict(name=args['name'], slug=slug))
-        return db['habits'].find_one(id=habit_id)
-
-class Entry(Resource):
-    def get(self, date):
-        return list(db['entries'].find(date=date))
-
-    def post(self, date):
-        parser = reqparse.RequestParser()
-        parser.add_argument('habit', type=str, action='append', required=True)
-        args = parser.parse_args()
-
-        entry = dict()
-        entry['date'] = date
-        for habit in args['habit']:
-            entry[habit] = True
-        entry_id = db['entries'].insert(entry)
-        return db['entries'].find_one(id=entry_id)
-
-api.add_resource(Habit, '/<string:slug>')
-api.add_resource(Entry, '/entries/<string:date>')
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
