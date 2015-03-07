07b953fe042b7a5d17abdfec59c388e3bf666ec2
Christopher Su <christophersu9@gmail.com>
2015-03-06 16:29:05 -0800

    reorg

--- habits/server.py
+++ habits/server.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/api')
 
 @app.route('/')
-def main():
+def index():
     return app.send_static_file('app.html')
+
+def production():
+    import logging
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+    app.run(debug=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
